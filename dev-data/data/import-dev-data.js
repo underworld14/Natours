6a71ee2b270e8461bb2da84fc9cfe930cdb5a527
zaulsr <zaulsr14@gmail.com>
2020-01-30 22:23:37 +0700

    add import and delete db script

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,51 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+const Tours = require('../../models/tours');
+
+// env configuration file
+dotenv.config({ path: './config.env' });
+
+const { DATABASE } = process.env;
+
+// connecting to database
+mongoose
+  .connect(DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('connection successfull'))
+  .catch(err => console.log(err));
+
+// read all tours data from the file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const deleteData = () => {
+  Tours.deleteMany()
+    .then(() => {
+      console.log('Data successfull deleted');
+      process.exit();
+    })
+    .catch(err => console.log(err));
+};
+
+const importData = () => {
+  Tours.create(tours)
+    .then(() => {
+      console.log('Data successfull imported');
+      process.exit();
+    })
+    .catch(err => console.log(err));
+};
+
+// option on running file
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
