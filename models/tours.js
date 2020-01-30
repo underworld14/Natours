5c5b9dfbc48eccd7bc3ef11d51a9a56cb5462a3b
zaulsr <zaulsr14@gmail.com>
2020-01-30 22:02:41 +0700

    reconfigured data model

--- models/tours.js
+++ models/tours.js
@@ -6,13 +6,46 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     unique: true
   },
-  rating: {
+  duration: {
     type: Number,
-    default: 4.0
+    required: [true, 'A tour must have duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have maxGroupSize']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have difficulty']
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 3.0
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price']
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: String,
+  images: [String],
+  startDates: [Date],
+  createdAt: {
+    type: Date,
+    default: Date.now()
   }
 });
 
