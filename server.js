84528b1a527838125f610d681fa5d33b53adbbe5
zaulsr <zaulsr14@gmail.com>
2020-01-30 09:55:46 +0700

    connecting mongoose to atlas

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 // env configuration file
 dotenv.config({ path: './config.env' });
 
+const { DATABASE } = process.env;
+
+mongoose
+  .connect(DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('connection successfull'))
+  .catch(err => console.log(err));
+
 const server = require('./app');
 
 const port = process.env.PORT || 8000;
-
-// console.log(process.env);
 
 // start the server
 server.listen(port, () => {
