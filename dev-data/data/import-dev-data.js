518a7b0c83463290a198218f3f83964faaa7ccd8
zaulsr <zaulsr14@gmail.com>
2020-02-24 02:19:07 +0700

    bulk insert db

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 
 const Tours = require('../../models/toursModels');
+const Users = require('../../models/usersModel');
+const Reviews = require('../../models/reviewsModels');
 
 // env configuration file
 dotenv.config({ path: './config.env' });
@@ -22,23 +24,33 @@ mongoose
 
 // read all tours data from the file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
-const deleteData = () => {
-  Tours.deleteMany()
-    .then(() => {
-      console.log('Data successfull deleted');
-      process.exit();
-    })
-    .catch(err => console.log(err));
+const deleteData = async () => {
+  try {
+    await Users.deleteMany();
+    await Tours.deleteMany();
+    await Reviews.deleteMany();
+
+    console.log('Data successfull deleted');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
 };
 
-const importData = () => {
-  Tours.create(tours)
-    .then(() => {
-      console.log('Data successfull imported');
-      process.exit();
-    })
-    .catch(err => console.log(err));
+const importData = async () => {
+  try {
+    await Users.create(users, { validateBeforeSave: false });
+    await Tours.create(tours);
+    await Reviews.create(reviews);
+
+    console.log('Data successfull deleted');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
 };
 
 // option on running file
