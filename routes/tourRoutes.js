83becd8bc56d24db4d3a217af3966251aba1d480
zaulsr <zaulsr14@gmail.com>
2020-03-03 16:13:43 +0700

    add multer and refactor

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,44 +5,47 @@ const router = express.Router();
 // require the controllers
 const authControllers = require('../controllers/authController');
 const toursControllers = require('../controllers/tourControllers');
-// const reviewsControllers = require('../controllers/reviewController');
 const { upToursImg } = require('../utils/uploadImage');
 
 const { protected, restrictTo } = authControllers;
+const {
+  processTourImg,
+  postTour,
+  getAllTours,
+  getTourById,
+  updateTour,
+  deleteTour,
+  aliasTopCheaps,
+  getToursStats,
+  getMonthlyPlan,
+  getWithIn,
+  getDistances
+} = toursControllers;
 
 // params middleware
 // router.param("id", toursControllers.checkId);
 
 // review router and nesting are here !!
 const reviewRoutes = require('../routes/reviewRoutes');
-// post tour/:tourId/reviews
+
 router.use('/:tourId/reviews', reviewRoutes); //post tour review
 
 // our tour routes here
 router
   .route('/')
-  .get(toursControllers.getAllTours)
-  .post(protected, restrictTo('admin', 'lead-guide'), toursControllers.postTour);
-
-router.route('/tours-stats').get(toursControllers.getToursStats);
-router.route('/monthly-plan/:year').get(toursControllers.getMonthlyPlan);
-
-router.route('/top-cheap').get(toursControllers.aliasTopCheaps, toursControllers.getAllTours);
-
-router.get('/tours-within/:dist/:latlng/:unit', toursControllers.getWithIn);
-
-router.get('/get-distances/:latlng/:unit', toursControllers.getDistances);
+  .get(getAllTours)
+  .post(protected, restrictTo('admin', 'lead-guide'), upToursImg, processTourImg, postTour);
 
 router
   .route('/:id')
-  .get(toursControllers.getTourById)
-  .patch(
-    protected,
-    restrictTo('admin', 'lead-guide'),
-    upToursImg,
-    toursControllers.processTourImg,
-    toursControllers.updateTour
-  )
-  .delete(protected, restrictTo('admin', 'lead-guide'), toursControllers.deleteTour);
+  .get(getTourById)
+  .patch(protected, restrictTo('admin', 'lead-guide'), upToursImg, processTourImg, updateTour)
+  .delete(protected, restrictTo('admin', 'lead-guide'), deleteTour);
+
+router.route('/tours-stats').get(getToursStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
+router.route('/top-cheap').get(aliasTopCheaps, getAllTours);
+router.get('/tours-within/:dist/:latlng/:unit', getWithIn);
+router.get('/get-distances/:latlng/:unit', getDistances);
 
 module.exports = router;
